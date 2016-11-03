594ce89cc6d666dbcd7f0a2108d018b0cf006cd1
kviktor <kviktor@cloud.bme.hu>
2016-11-03 22:59:20 +0100

    add cache

--- server/web.py
+++ server/web.py
@@ -1,7 +1,16 @@
+import json
+import logging
+import sha
+
 from flask import Flask, request, jsonify
-from tasks import get_article_score
+from flask_redis import FlaskRedis
+from tasks import get_article_score, BROKER_URL
+
+logger = logging.getLogger(__name__)
 
 app = Flask(__name__)
+app.config['REDIS_URL'] = BROKER_URL
+redis_store = FlaskRedis(app)
 
 
 @app.route("/")
@@ -11,9 +20,25 @@ def home():
 
 @app.route('/get_scores/', methods=['GET', 'POST', ])
 def get_scores():
-    # TODO cache
     article_urls = request.get_json()
-    return jsonify(**{
-        article_url: get_article_score(article_url)
-        for article_url in article_urls
-    })
+    key = sha.new("".join(article_urls)).hexdigest()
+    result = redis_store.get(key)
+    if not result:
+        logger.info("%s not found in cache", key)
+        scores = {
+            article_url: get_article_score(article_url)
+            for article_url in article_urls
+        }
+        redis_store.set(key, json.dumps(scores))
+        redis_store.expire(key, 60 * 60 if all_w_dict(scores) else 30)
+    else:
+        scores = json.loads(result)
+
+    return jsonify(**scores)
+
+
+def all_w_dict(iterable_dict):
+    for i in iterable_dict.values():
+        if i is None:
+            return False
+    return True
