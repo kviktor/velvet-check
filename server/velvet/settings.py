5511161c91ce6fb663c00e1164237d4ba225f404
Viktor Kálmán <vkalman@consumeraffairs.com>
2020-04-18 18:41:14 +0200

    use db from env

--- server/velvet/settings.py
+++ server/velvet/settings.py
@@ -80,10 +80,7 @@ WSGI_APPLICATION = 'velvet.wsgi.application'
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+    'default': env.db(),
 }
 
 
